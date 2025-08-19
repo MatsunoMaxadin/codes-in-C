de5f235bbd2da49a903622875c4ac08eabb16693
Pedro Max Paz <155768614+MatsunoMaxadin@users.noreply.github.com>
2025-08-18 21:04:32 -0300

    Create calcMedia.c

---
+++ calcMedia.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+   int x;
+   int i;
+   int total = 0;
+   double media;
+   
+   printf("Quantos números você quer fazer a média? \n");
+   scanf("%d", &x);
+ 
+   int *arrNumbers = (int *)malloc(x*sizeof(int));
+   int *stationArr = arrNumbers;
+   
+   for(i = 0; i < x; i++){
+       fflush(stdin);
+       
+       printf("Digite o %dº número: ",i+1);
+       scanf(" %d", arrNumbers);
+       arrNumbers++;
+   };
+   
+   arrNumbers = stationArr;
+   
+  for(i = 0; i < x;  i++ ){
+      printf("O %dº número digitado foi o: %d \n", i+1, *arrNumbers);
+      total += *arrNumbers;
+      arrNumbers++;
+  };
+  
+  media = total/x;
+  
+  printf("A média dos números foi: %f, o total foi %d e a quantidade foi %d", media, total, x);
+  
+    return 0;
+}
