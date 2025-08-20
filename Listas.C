d8023efa7646d42bfc4b70eccf41a221d0e7dd67
Pedro Max Paz <155768614+MatsunoMaxadin@users.noreply.github.com>
2025-08-19 21:25:16 -0300

    Update Listas.C

--- Listas.C
+++ Listas.C
@@ -8,16 +8,19 @@ typedef struct FUNCTIONARY {
     struct FUNCTIONARY *nextFunc;
 } functionary;
 
-functionary* createList (functionary func, int age, float salary){
-    func.nextFunc = (functionary *) realloc(func.nextFunc, sizeof(functionary));
+functionary* putOnFinal (int age, float salary){
+    functionary* func = (functionary *) malloc(sizeof(functionary));
     
-    func.nextFunc->age = age;
-    func.nextFunc->salary = salary;
-    func.nextFunc->nextFunc = NULL;
+    func->age = age;
+    func->salary = salary;
+    func->nextFunc = NULL;
     
-    return func.nextFunc;
+    return func;
 
 };
+
+
+void putOnStart(functionary* first, functionary* second);
 
 void printFunctionary (functionary func){
     
@@ -25,27 +28,73 @@ void printFunctionary (functionary func){
     
 }
 
+void printAll(functionary* Start){
+    
+    functionary *tmp = Start;
+    
+    while(tmp->nextFunc != NULL){
+        printFunctionary(*(tmp->nextFunc));
+        tmp = tmp->nextFunc;
+    }
+}
+
+functionary* deleteList(functionary* LIST){
+    
+    LIST->nextFunc = NULL;
+    
+    return LIST;
+    
+    
+}
+
 int main(void){
     
     const int FunctionarySize = sizeof(functionary);
     functionary *LISTA = (functionary *) malloc(FunctionarySize);
-    LISTA->nextFunc = createList(*LISTA, 18, 1200.0);
+    LISTA->nextFunc = putOnFinal(18, 1200.0);
     
     functionary* Maxine = LISTA->nextFunc;
     
-    printFunctionary(*Maxine);
     
-   Maxine->nextFunc = createList(*Maxine, 19, 200.15);
+    
+    
+    
+   Maxine->nextFunc = putOnFinal(19, 200.15);
     
     functionary* newFunctionary = Maxine->nextFunc;
     
-    printFunctionary(*newFunctionary);
     
-    newFunctionary->nextFunc = createList(*newFunctionary, 21, 530.230);
+    newFunctionary->nextFunc = putOnFinal(21, 530.230);
     
     functionary* newFunctionaryTwo = newFunctionary->nextFunc;
+   
+    printFunctionary(*(LISTA->nextFunc));
+    printFunctionary(*(Maxine->nextFunc));
+    printFunctionary(*(newFunctionary->nextFunc));
     
-    printFunctionary(*newFunctionaryTwo);
-
+    // apenas testando
+    
+    functionary* armazem;
+    
+    armazem = newFunctionaryTwo;
+    
+    newFunctionary->nextFunc = putOnFinal(33, 4200.15);
+    functionary* newFunctionaryOne = newFunctionary->nextFunc;
+    newFunctionaryOne->nextFunc = armazem;
+    
+    printf("---------\n");
+    
+    printFunctionary(*(LISTA->nextFunc));
+    printFunctionary(*(Maxine->nextFunc));
+    printFunctionary(*(newFunctionary->nextFunc));
+    printFunctionary(*(newFunctionaryOne->nextFunc));
+    
+    printf("--------\n");
+    
+    printAll(LISTA);
+    
     return 0;
+    
+
+
 }
